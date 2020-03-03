1dce5c479d6e02e906e5fe8102cd2b2181589472
Victor Ezeh <ezehgvictor@gmail.com>
2020-03-03 15:56:11 +0100

    Create Idea Data Model

---
+++ src/idea/idea.entity.ts
@@ -0,0 +1,9 @@
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, Column } from "typeorm";
+
+@Entity('idea')
+export class IdeaEntity{
+    @PrimaryGeneratedColumn('uuid')id:string
+    @CreateDateColumn() created:Date;
+    @Column('text') idea:string;
+    @Column('text') description:string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+
+const port = process.env.PORT || 8080
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap')
 }
 bootstrap();
