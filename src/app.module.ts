83cd8bbc21ba90be5687982d4114c513218dd885
Victor Ezeh <ezehgvictor@gmail.com>
2020-03-08 01:04:17 +0100

    Idea Crud Api Basic Settup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import {TypeOrmModule} from '@nestjs/typeorm'
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdeaModule } from './idea/idea.module';
+import { IdeaEntity } from './idea/idea.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db',
+    entities: [IdeaEntity],
+    synchronize: true,
+  }), IdeaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/idea/idea.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { IdeaController } from './idea.controller';
+import { IdeaService } from './idea.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IdeaEntity } from './idea.entity';
+
+@Module({
+  imports:[TypeOrmModule.forFeature([IdeaEntity])],
+  controllers: [IdeaController],
+  providers: [IdeaService],
+  exports: [TypeOrmModule]
+})
+export class IdeaModule {}
